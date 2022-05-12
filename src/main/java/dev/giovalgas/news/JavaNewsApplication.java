6f007394557a472d6bad4ed14d6d69cf0194f854
giovalgas <giovalgascom@gmail.com>
2022-05-12 06:49:30 -0300

    Initial commit

---
+++ src/main/java/dev/giovalgas/news/JavaNewsApplication.java
@@ -0,0 +1,13 @@
+package dev.giovalgas.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNewsApplication.class, args);
+	}
+
+}
